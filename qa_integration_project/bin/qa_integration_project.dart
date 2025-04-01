b7f84958420ce9b85c5d251421e290088305baea
Ashfaq Adil <ashfaqrahmanadil@gmail.com>
2025-03-31 22:29:29 -0400

    Added GitHub Actions workflow

---
+++ qa_integration_project/bin/qa_integration_project.dart
@@ -0,0 +1,5 @@
+import 'package:qa_integration_project/qa_integration_project.dart' as qa_integration_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${qa_integration_project.calculate()}!');
+}
---
+++ qa_integration_project/test/qa_integration_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:qa_integration_project/qa_integration_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
